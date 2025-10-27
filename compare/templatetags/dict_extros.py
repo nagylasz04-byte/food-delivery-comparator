56f3f7b855b4a663fcaf0b93b639961e0a3eafb1
Nagy László <nagylasz04@gmail.com>
2025-10-27 22:58:15 +0100

    felesleges fileok hibák javítása követekező, doc megítása, magyar kommentek írása

--- compare/templatetags/dict_extros.py
+++
@@ -1,9 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter
-def dict_get(d, key):
-    """Safe dict getter for templates: {{ mydict|dict_get:key }}"""
-    if not d:
-        return None
-    return d.get(key)
